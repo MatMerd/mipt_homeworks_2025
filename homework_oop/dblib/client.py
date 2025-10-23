39434757e59d77bf11cb499dc2ee81d45f163e75
Alex <kennyobla123@gmail.com>
2025-10-23 19:22:43 +0300

    add client class and example of its usage in main

---
+++ homework_oop/dblib/client.py
@@ -0,0 +1,92 @@
+from dataclasses import dataclass, field
+from .db_connect import *
+
+
+@dataclass
+class Client:
+    connections : dict[int, DBConnect] = field(default_factory=dict[int, DBConnect])
+    saved_queries : list[tuple] = field(default_factory=list[tuple])
+    _total_connections_count = 0
+
+    def _GetNewIndex(self):
+        self._total_connections_count += 1
+        return self._total_connections_count
+
+    def Connect(self, file_name : str):
+        self.connections[self._GetNewIndex()] = DBConnect(file_name)
+
+    def Close(self, ind : int):
+        del self.connections[ind]
+
+    class NoConnectionFound(Exception):
+        def __init__(self, error):
+            self.error = error
+        def __str__(self):
+            return self.error
+
+    class InvalidArgumentNumber(Exception):
+        def __init__(self, error):
+            self.error = error
+        def __str__(self):
+            return self.error
+
+    class InvalidArgument(Exception):
+        def __init__(self, error):
+            self.error = error
+        def __str__(self):
+            return self.error
+
+    @dataclass
+    class DataManager:
+        data : list[tuple]
+        headers_names : list[str]
+
+        class InvalidArgument(Exception):
+            def __init__(self, error):
+                self.error = error
+
+            def __str__(self):
+                return self.error
+
+        def Select(self, select : list[str]):
+            query = []
+            for col_name in select:
+                if not col_name in self.headers_names:
+                    raise self.InvalidArgument(f"No column named {col_name}")
+            for data_line in self.data:
+                new_line = []
+                for ind, col_name in enumerate(self.headers_names):
+                    if col_name in select:
+                        new_line.append(data_line[ind])
+                query.append(new_line)
+            return query
+
+        def Print(self):
+            return self.data
+
+    def Query(self, connection_ind : int = None, where_val : list[tuple] = None, where_col : list[tuple] = None, order_by : list[tuple] = None):
+        if connection_ind is None or not connection_ind in self.connections:
+            raise self.NoConnectionFound(f"No connect with {connection_ind=}")
+        connection = self.connections[connection_ind]
+        for where_query in where_val:
+            if len(where_query) != 3:
+                raise self.InvalidArgumentNumber(f"where_val expected 3 arguments, got {len(where_query)} : {where_query}")
+            if where_query[1] not in ["<", "<=", ">", ">=", "=="]:
+                raise self.InvalidArgument(f"Not suitable operator {where_query[1]}")
+            connection.add_query(("WHERE", where_query[1], where_query[0], where_query[2]))
+        for where_query in where_col:
+            if len(where_query) != 3:
+                raise self.InvalidArgumentNumber(f"where_cal expected 3 arguments, got {len(where_query)} : {where_query}")
+            if where_query[1] not in ["<", "<=", ">", ">=", "=="]:
+                raise self.InvalidArgument(f"Not suitable operator {where_query[1]}")
+            connection.add_query(("WHERECOL", where_query[1], where_query[0], where_query[2]))
+        for order_by_query in order_by:
+            if len(order_by_query) != 2:
+                raise self.InvalidArgumentNumber(f"order_by expected 2 arguments, got {len(order_by_query)} : {order_by_query}")
+            if order_by_query[1] not in [True, False]:
+                raise self.InvalidArgument(f"The sorting order in order_by is not clear : {order_by_query[1]} not a boolean True or False")
+            if order_by_query[1]:
+                connection.add_query(("SORT", "r"))
+            else:
+                connection.add_query(("SORT", order_by_query[0], "n"))
+        return self.DataManager(connection.execute(), connection.get_headers())
