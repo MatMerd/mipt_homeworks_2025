60ecfbeeddef5b5486bb4072e2ba1b344dd2d77c
plaschinskij nikolaj <niccneimi@gmail.com>
2025-10-25 19:57:12 +0300

    feat: реализован класс для выбора, сортировки и группировки данных

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
-from homework_oop.CSVReader import CSVReader 
+from homework_oop.CSVReader import CSVReader
+from homework_oop.DataProcessor import DataProcessor
 
 DATA_PATH = "homework_oop/repositories.csv"
 
 def main():
     repositories = CSVReader(DATA_PATH)
     repositories.read()
-    print(repositories.get_column_names())
+    
+    processor = DataProcessor(repositories)
+    result = (processor.select('Name', 'Language', 'Stars').sort_by('Stars', reverse=True).execute())
+    
+    for repo in result[:10]:
+        print(f"{repo['Name']} ({repo['Language']}): {repo['Stars']}")
 
 if __name__ == "__main__":
     main()
