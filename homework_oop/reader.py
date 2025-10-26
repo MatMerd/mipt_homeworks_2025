6c93c35822b30b85edda82493908b2dc74238bad
syarenkova <syarenkova@gmail.com>
2025-10-26 19:22:27 +0300

    fix(hw1): fixed reader

--- homework_oop/reader.py
+++ homework_oop/reader.py
@@ -6,17 +6,13 @@ from typing import List
 
 class CSVRepositoryReader:
     def __init__(self, file_path: str, encoding: str = "utf-8"):
-        self.file_path = file_path
+        self.path = self._get_file_path(file_path)
         self.encoding = encoding
         self.data: List[Repository] = []
 
     def read(self) -> "CSVRepositoryReader":
-        path = Path(self.file_path)
-        if not path.exists():
-            raise FileNotFoundError(f"CSV file not found: {self.file_path}")
-
         repositories: list[Repository] = []
-        with path.open(encoding=self.encoding) as csv_file:
+        with self.path.open(encoding=self.encoding) as csv_file:
             reader = csv.DictReader(csv_file)
             for row in reader:
                 repositories.append(Repository.from_csv_row(row))
@@ -25,3 +21,12 @@ class CSVRepositoryReader:
 
     def get_data(self) -> list[Repository]:
         return list(self.data)
+
+    def _get_file_path(self, file_path: str) -> Path:
+        path = Path(file_path)
+        self._check_file_path(path)
+        return path
+
+    def _check_file_path(self, path: Path):
+        if not path.exists():
+            raise FileNotFoundError(f"CSV file not found: {path}")
