b62f3c92a8c2bff88a265488e16453aa0db2224a
pr0stSasha <108032663+pr0stSasha@users.noreply.github.com>
2025-12-23 21:30:18 +0300

    поменяла декоратор. теперь верный наверное

--- logger_utils.py
+++ logger_utils.py
@@ -3,15 +3,20 @@ import functools
 from typing import Optional, Callable, Any
 
 def log_errors(logger: Optional[logging.Logger] = None):
+    """
+    Декоратор, который логирует ошибки. 
+    Принимает опциональный объект логгера.
+    """
     def decorator(func: Callable[..., Any]):
-        
         @functools.wraps(func)
         def wrapper(*args: Any, **kwargs: Any) -> Any:
             try:
                 return func(*args, **kwargs)
             except Exception as e:
                 current_logger = logger if logger is not None else logging.getLogger(func.__module__)
-                current_logger.error(f"Ошибка в {func.__name__}: {e}", exc_info=True)
+                
+                current_logger.error(f"Ошибка в функции {func.__name__}: {e}", exc_info=True)
+                
                 raise e
         return wrapper
     return decorator
